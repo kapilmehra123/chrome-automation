c994af97672ab7fd2dc4e2edf4a3efdeafcd5ce6
kapil <studyanywhere6@gmail.com>
2024-03-27 11:39:48 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -1,15 +1,14 @@
-# Download and Install chromedriver
-wget -N https://chromedriver.storage.googleapis.com/121.0.6167.184/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+# Install the latest version of Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
 
+# Install the latest version of ChromeDriver
+CHROME_VERSION=$(google-chrome-stable --version | grep -oE '[0-9]{2}')
+LATEST_CHROMEDRIVER_VERSION=$(wget -q -O - https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+wget https://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/
 
-# Install chrome broswer
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-apt-get -y update
-apt-get -y install google-chrome-stable
-
+# Clean up downloaded files
+rm chromedriver_linux64.zip google-chrome-stable_current_amd64.deb
