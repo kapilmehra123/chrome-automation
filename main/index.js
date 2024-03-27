949ce75de806d7f78d2301f11ee2b0caef000d29
kapil <studyanywhere6@gmail.com>
2024-03-27 10:48:16 +0530

    .

--- main/index.js
+++ main/index.js
@@ -25,7 +25,15 @@ const {
 } = require('./loadfile');
 let options = new chrome.Options();
 //for the headless mode
-options.addArguments("--headless");
+
+options.addArguments("headless"); // headless -> no browser window. needed for jenkins
+options.addArguments("disable-infobars"); // disabling infobars
+options.addArguments("--disable-extensions"); // disabling extensions
+options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+options.addArguments("--no-sandbox"); // Bypass OS security model
+
+
+driver.get("www.google.com");
 //for the headless mode
 const driver = new Builder().forBrowser("chrome").setChromeOptions(options).build();
 const path = require('path');
