5ce2ebac1114695a2c00f22e77af3d3e0c41a1d5
kapil <studyanywhere6@gmail.com>
2024-03-27 17:34:16 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -1,8 +1,13 @@
+# Download and Install chromedriver
+wget -N https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip -P ~/
+unzip ~/chromedriver-linux64.zip -d ~/
+rm ~/chromedriver-linux64.zip
 
-npm install chromedriver
+
 
 # Install chrome broswer
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 apt-get -y update
 apt-get -y install google-chrome-stable
+
