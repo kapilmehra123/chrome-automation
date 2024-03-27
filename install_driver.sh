21591979f49e50c597f538a97bc1c2b2252a2ab2
kapil <studyanywhere6@gmail.com>
2024-03-27 17:54:04 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -2,12 +2,41 @@
 wget -N https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip -P ~/
 unzip ~/chromedriver-linux64.zip -d ~/
 rm ~/chromedriver-linux64.zip
-
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
 
 
 # Install chrome broswer
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-apt-get -y update
-apt-get -y install google-chrome-stable
+apt-get update && apt-get install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg \
+hicolor-icon-theme \
+libcanberra-gtk* \
+libgl1-mesa-dri \
+libgl1-mesa-glx \
+libpango1.0-0 \
+libpulse0 \
+libv4l-0 \
+fonts-symbola \
+--no-install-recommends \
+&& curl -sSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
+&& echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list \
+&& apt-get update && apt-get install -y \
+google-chrome-stable \
+--no-install-recommends \
+&& apt-get purge --auto-remove -y curl \
+&& rm -rf /var/lib/apt/lists/*
 
+set -x \
+&& apt-get update \
+&& apt-get install -y --no-install-recommends \
+ca-certificates \
+curl \
+unzip \
+&& rm -rf /var/lib/apt/lists/* \
+&& curl -sSL "https://dl.google.com/linux/direct/google-talkplugin_current_amd64.deb" -o /tmp/google-talkplugin-amd64.deb \
+&& dpkg -i /tmp/google-talkplugin-amd64.deb
+ADD https://dl.google.com/linux/direct/google-talkplugin_current_amd64.deb /src/google-talkplugin_current_amd64.deb
