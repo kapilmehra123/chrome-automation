16618b547e834b4ef05d7aaa9f826a581fd0880e
kapil <studyanywhere6@gmail.com>
2024-03-27 19:13:49 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -1,7 +1,7 @@
 # Download and Install chromedriver
-wget -N https://chromedriver.storage.googleapis.com/71.0.3578.80/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
+wget -N https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip -P ~/
+unzip ~/chromedriver-linux64.zip -d ~/
+rm ~/chromedriver-linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
