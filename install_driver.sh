53ca4cb2a8afd6ec0d6cbc389ba71c66cb19fe8c
kapil <studyanywhere6@gmail.com>
2024-03-27 18:34:07 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -8,35 +8,6 @@ sudo chmod 0755 /usr/local/bin/chromedriver
 
 
 # Install chrome broswer
-apt-get update && apt-get install -y \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg \
-hicolor-icon-theme \
-libcanberra-gtk* \
-libgl1-mesa-dri \
-libgl1-mesa-glx \
-libpango1.0-0 \
-libpulse0 \
-libv4l-0 \
-fonts-symbola \
---no-install-recommends \
-&& curl -sSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
-&& echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list \
-&& apt-get update && apt-get install -y \
-google-chrome-stable \
---no-install-recommends \
-&& apt-get purge --auto-remove -y curl \
-&& rm -rf /var/lib/apt/lists/*
-
-set -x \
-&& apt-get update \
-&& apt-get install -y --no-install-recommends \
-ca-certificates \
-curl \
-unzip \
-&& rm -rf /var/lib/apt/lists/* \
-&& curl -sSL "https://dl.google.com/linux/direct/google-talkplugin_current_amd64.deb" -o /tmp/google-talkplugin-amd64.deb \
-&& dpkg -i /tmp/google-talkplugin-amd64.deb
-ADD https://dl.google.com/linux/direct/google-talkplugin_current_amd64.deb /src/google-talkplugin_current_amd64.deb
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
