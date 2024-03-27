a307294c17abc58292e225e206da5aaa82ae88d1
kapil <studyanywhere6@gmail.com>
2024-03-27 19:22:38 +0530

    >

--- install_driver.sh
+++ install_driver.sh
@@ -2,10 +2,9 @@
 wget -N https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip -P ~/
 unzip ~/chromedriver-linux64.zip -d ~/
 rm ~/chromedriver-linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
-
+sudo mv chromedriver /usr/bin/chromedriver 
+sudo chown root:root /usr/bin/chromedriver 
+sudo chmod +x /usr/bin/chromedriver 
 
 # Install chrome broswer
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
