a0cc547a8856105c65b4d7e50e5f338cae016635
kapil <studyanywhere6@gmail.com>
2024-03-27 17:12:05 +0530

    .

--- install_driver.sh
+++ install_driver.sh
@@ -1,9 +1,5 @@
 
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
+Install-Package Selenium.WebDriver.ChromeDriver -Version 123.0.6312.86
 
 # Install chrome broswer
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
