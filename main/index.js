262dc3ebc75b7647760cd1f3b13358593df463f9
kapil <studyanywhere6@gmail.com>
2024-04-11 18:44:59 +0530

    .

--- main/index.js
+++ main/index.js
@@ -66,7 +66,7 @@ describe("login page", function () {
     await driver.navigate().refresh();
     log(':step 4')
   })
- 
+ /*
   it("Upload a csv file", async function () {
       log(':step 5:starting Upload a csv file')
       //click on nav bar icon
@@ -2595,4 +2595,4 @@ describe("login page", function () {
      })
     */
   })
-})
+//})
